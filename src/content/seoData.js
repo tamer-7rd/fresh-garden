1f679c6431ded90125a562972b542ff672e5e3ae
Tamerlan <tamerlan4496@gmail.com>
2026-01-12 19:00:28 +0400

    fix: Open Graph Image

--- src/content/seoData.js
+++ src/content/seoData.js
@@ -10,7 +10,7 @@ export const seoData = {
         type: 'website',
         author: 'Fresh Garden',
         themeColor: '#22c55e',
-        defaultImage: '/og-image.jpg'
+        defaultImage: '/og-image.png'
     },
 
     // Page-specific SEO
@@ -66,7 +66,7 @@ export const seoData = {
         '@type': 'LocalBusiness',
         '@id': 'https://freshgardenquba.az/#business',
         name: 'Fresh Garden',
-        image: 'https://freshgardenquba.az/og-image.jpg',
+        image: 'https://freshgardenquba.az/og-image.png',
         telephone: '+994-55-660-10-50',
         email: 'freshgardenquba.mmc@gmail.com',
         address: {
